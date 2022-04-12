31bd821adc840f4c3e0e5cfbaa0bfc4004c989d5
Tzu-Hung Liang <t110318094@ntut.org.tw>
2022-04-12 17:04:34 +0800

    [Updated]

--- playground.py
+++ playground.py
@@ -3,6 +3,7 @@ from tqdm import tqdm
 
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt 
 
 import utils
 
@@ -12,22 +13,47 @@ dates = ['20200929', '20201001', '20201003', '20201005', '20201007',
 
 opt = utils.Config()
 
-svm_layouts = {
-        'lSVM1': 0.01,
-        'lSVM2': 1,
-        'lSVM3': 100,
-        'kSVM1': [0.01, 0.001],
-        'kSVM2': [0.01, 'scale'],
-        'kSVM3': [0.01, 'auto'],
-        'kSVM4': [1, 0.001],
-        'kSVM5': [1, 'scale'],
-        'kSVM6': [1, 'auto'],
-        'kSVM7': [100, 0.001],
-        'kSVM8': [100, 'scale'],
-        'kSVM9': [100, 'auto']
+svm_derivative = {
+        'lSVM1': 0.99,
+        'lSVM2': 0.2,
+        'lSVM3': 0.15,
+        'kSVM1': 0.6,
+        'kSVM2': 0.89,
+        'kSVM3': 0.45,
+        'kSVM4': 0.10,
+        'kSVM5': 0.85,
+        'kSVM6': 0.42,
+        'kSVM7': 0.26,
+        'kSVM8': 0.75,
+        'kSVM9': 0.92
     }
 
-# for svm_layout_id, params_list in tqdm(svm_layouts.items()):
-#     print(svm_layout_id, params_list)
+svm_reflectance = {
+        'lSVM1': 0.4,
+        'lSVM2': 0.2,
+        'lSVM3': 0.15,
+        'kSVM1': 0.45,
+        'kSVM2': 0.75,
+        'kSVM3': 0.32,
+        'kSVM4': 0.05,
+        'kSVM5': 0.8,
+        'kSVM6': 0.21,
+        'kSVM7': 0.1,
+        'kSVM8': 0.66,
+        'kSVM9': 0.78
+    }
 
-utils.save_dict(svm_layouts, opt.mushroom_class)
+x = ['lSVM1', 'lSVM2', 'lSVM3', 'kSVM1', 'kSVM2', 'kSVM3', 'kSVM4', 'kSVM5', 'kSVM6', 'kSVM7', 'kSVM8', 'kSVM9']
+x_axis = np.arange(len(x))
+svm_der = list(svm_derivative.values())
+svm_ref = list(svm_reflectance.values())
+
+plt.bar(x_axis - 0.2, svm_der, 0.4, label = 'Derivative Spectrum')
+plt.bar(x_axis + 0.2, svm_ref, 0.4, label = 'Reflectance Spectrum')
+  
+plt.xticks(x_axis, x)
+plt.xlabel("Models")
+plt.ylabel("Accuracy")
+plt.title("")
+plt.legend()
+plt.show()
